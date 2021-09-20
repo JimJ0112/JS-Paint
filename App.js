849beeedf15e907e449392a83aacecd51d0b33b4
JimJ0112 <68044290+JimJ0112@users.noreply.github.com>
2021-09-20 18:32:32 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,163 @@
+window.addEventListener("load",MainFunc);
+
+
+
+//---------------------------------------------------------------------
+
+
+// Global Variables
+const PaintCanvas = document.getElementById("PaintCanvas");
+const ctx_PaintCanvas = PaintCanvas.getContext('2d');
+var LocationX;
+var LocationY;
+let StartLocationX , StartLocationY , EndLocationX,EndLocationY;
+let isPainting = false;
+var DisplayLocationX = document.getElementById("BottomPanel-XLocation");
+var DisplayLocationY = document.getElementById("BottomPanel-YLocation");
+ var StackData = new Array();
+ var UndoData = new Array();
+ var RedoData = new Array();
+ var counter = 5;
+ var CanvasData;
+
+
+// Main function
+function MainFunc(){
+   
+    console.log("Js loaded");
+    console.log(ctx_PaintCanvas);
+    PaintCanvas.width = window.innerWidth;
+    PaintCanvas.height = window.innerHeight;
+
+    let cWidth = PaintCanvas.width;
+    let cHeight = PaintCanvas.height;
+    /*
+    // adding a grid to the canvas
+    ctx_PaintCanvas.strokeStyle = "#ddd";
+    for(var x = 0.5; x <= cWidth; x+=10){
+        ctx_PaintCanvas.moveTo(x, 0);
+        ctx_PaintCanvas.lineTo(x, cHeight);
+    }
+
+    for(var y = 0.5; y <= cHeight; y+=10){
+        ctx_PaintCanvas.moveTo(0, y);
+        ctx_PaintCanvas.lineTo(cWidth, y);
+    }
+    ctx_PaintCanvas.strokeStyle = "#ddd";
+    ctx_PaintCanvas.stroke();
+   */
+   
+
+    // Event listeners
+
+  
+
+PaintCanvas.addEventListener("mousedown",StartLoc);
+PaintCanvas.addEventListener("mouseup",FinishLoc);
+PaintCanvas.addEventListener("mousemove",Draw);
+window.addEventListener("resize",function(){     PaintCanvas.width = window.innerWidth;
+    PaintCanvas.height = window.innerHeight;})
+
+    window.addEventListener("keydown",function(){
+       
+        if(event.code == "KeyA"){
+            console.log(event.code);
+            ctx_PaintCanvas.clearRect(0,0,PaintCanvas.width,PaintCanvas.height);
+            var img = new Image;
+           // img.src = CanvasData;
+           img.src = UndoData.pop();
+           RedoData.push(img);
+            img.onload = function(){
+                
+                ctx_PaintCanvas.drawImage(img,0,0,PaintCanvas.width,PaintCanvas.height);
+            }
+        }
+
+        if(this.event.code == "KeyS"){
+            
+            img.src = RedoData.pop();
+            img.onload = function(){
+                ctx_PaintCanvas.drawImage(img,0,0,PaintCanvas.width,PaintCanvas.height);
+            }
+        }
+
+        if(this.event.code == "KeyT"){
+            document.getElementById("ToolsPanel").style.display="none";
+            document.getElementById("BottomPanel").style.display = "none";
+        }
+
+        if(this.event.code=="KeyY"){
+            document.getElementById("ToolsPanel").style.display="grid";
+            document.getElementById("BottomPanel").style.display = "grid";
+        }
+
+    });
+
+}
+
+//functions
+
+
+function StartLoc(e){
+    
+    isPainting = true;
+    LocationX = e.clientX;
+    LocationY = e.clientY;
+ 
+    /*
+    StartLocationX = e.clientX;
+    StartLocationY = e.clientY;
+    */
+   
+    
+  
+}
+
+function FinishLoc(e){
+    isPainting= false;
+   ctx_PaintCanvas.beginPath();
+   EndLocationX = e.clientX;
+   EndLocationY = e.clientY;
+  /* StackData[0] = PaintCanvas.toDataURL("image/png"); */
+   CanvasData = PaintCanvas.toDataURL("image/png");
+
+   
+UndoData.push(PaintCanvas.toDataURL("image/png"));
+//UndoData[counter] = PaintCanvas.toDataURL("image/png");
+
+console.log(counter);
+  
+
+}
+
+function Draw(e){
+   // /*
+if(isPainting){
+    ctx_PaintCanvas.strokeStyle = "black";
+    LocationX = e.clientX;
+    LocationY = e.clientY;
+
+
+    DisplayLocationX.textContent = LocationX;
+    DisplayLocationY.textContent = LocationY;
+    ctx_PaintCanvas.lineWidth = 1;
+    ctx_PaintCanvas.lineCap = "round";
+
+    ctx_PaintCanvas.lineTo(LocationX , LocationY);
+    
+ 
+    ctx_PaintCanvas.stroke();
+    
+
+
+} else{//console.log("isPainting? " + isPainting);
+
+}
+
+//*/
+// experiment
+
+
+
+}
+
